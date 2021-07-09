7980ecc0cd3d9976507396b63a0924284662790a
BooRiis <martin.javorsky456@gmail.com>
2021-07-09 22:16:12 +0200

    uploading and preparing project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom"
+import {useForm} from 'react-hook-form'
+
+
+
+
+//assets
+import '../node_modules/normalize.css/normalize.css'
+import './assets/css/style.scss'
+
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Allinone/stepNumberOne.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+
+function stepNumberOne() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default stepNumberOne

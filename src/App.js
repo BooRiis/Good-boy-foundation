c0029281ee4fe24110366f66de05a8f08987c07a
BooRiis <martin.javorsky456@gmail.com>
2021-07-10 13:27:42 +0200

    added router in App.js and working on stepNumberOne.js I already added small nav dots

--- src/App.js
+++ src/App.js
@@ -11,13 +11,34 @@ import {useForm} from 'react-hook-form'
 //assets
 import '../node_modules/normalize.css/normalize.css'
 import './assets/css/style.scss'
+import bg from './assets/imgs-icons/doggo.png'
+
+//components
+import {NavBar} from './components/NavBar'
+import {stepNumberOne} from './components/Allinone/stepNumberOne'
+import {stepNumberTwo} from './components/Allinone/stepNumberTwo'
+import {stepNumberThree} from './components/Allinone/stepNumberThree'
+import {Footer} from './components/Footer'
 
 
 function App() {
   return (
-    <div>
-      
-    </div>
+      <Router>
+      <NavBar />
+        <div className="router-section">
+          <main>
+            <div className="route-form">
+              <Route exact path="/" component={stepNumberOne} />
+              <Route exact path="/step-two" component={stepNumberTwo} />
+              <Route exact path="/step-three" component={stepNumberThree} />
+            </div>
+            <div className="background-image">
+              <img src={bg} alt="" />
+            </div>
+          </main>
+          <Footer />
+        </div>
+      </Router>
   );
 }
 
--- src/components/Allinone/stepNumberOne.js
+++ src/components/Allinone/stepNumberOne.js
@@ -2,12 +2,20 @@ import React from 'react'
 import { useHistory } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 
-function stepNumberOne() {
+// assets
+import '../../../node_modules/normalize.css/normalize.css'
+import '../../assets/css/style.scss'
+
+export function stepNumberOne() {
     return (
         <div>
-            
+            <div className="nav">
+                <span className="active"></span>
+                <span></span>
+                <span></span>
+            </div>
+            <h1>Vyberte možnosť, ako chcete pomôcť</h1>
         </div>
     )
 }
 
-export default stepNumberOne
